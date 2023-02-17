9bb1b04aada47351e57c7c56bc9a2f06cd4578ad
Adrian Montano <adrian.montano2011@yahoo.com>
2023-02-17 14:36:59 -0400

    Aumento de eleccion y vista del estado seleccionado

---
+++ src/f_estado.js
@@ -0,0 +1,10 @@
+const select = document.querySelector("#estado");
+const form = document.querySelector("#estado-form");
+const div = document.querySelector("#estado-div");
+
+form.addEventListener("submit", (event)=> {
+    event.preventDefault();
+
+    const estado = select.value;
+    div.textContent = "Estado: " + estado;
+});
